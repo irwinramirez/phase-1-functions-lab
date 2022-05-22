f67b618bb7cac756e4af079d6fbcc27528fdd9a8
Irwin Ramirez <Irwinramirez23@gmail.com>
2022-05-22 17:16:41 -0400

    commit tree ca1a68b84d9759d1d274595bddce9c3dacf160a0

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(42-pickupLocation);
+}
+function distanceFromHqInFeet(pickupLocation){
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+function distanceTravelledInFeet(start, destination){
+    if(start > destination){
+        return (start - destination) * 264;
+    }
+    else{
+        return (destination - start)* 264;
+    } 
+
+}
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet (start, destination);
+    if(distance < 400){
+        return 0
+    }
+    else if (distance > 400 && distance <= 2000 ){
+        return .02 * (distance - 400)
+    }
+    else if (distance > 2000 && distance < 2500){
+        return 25
+    }
+    else if (distance > 2500){
+        return "cannot travel that far"
+    }
+}
